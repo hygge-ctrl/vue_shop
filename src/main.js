0fcdeb50c66aedb35b96cf9e28081ea65de3db7a
hygge <1525464489@qq.com>
2020-01-27 16:52:36 +0800

    完成了登录功能

--- src/main.js
+++ src/main.js
@@ -2,7 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
+// 导入字体图标
+import './assets/fonts/iconfont.css'
+// 倒入全局样式表
+import './assets/global.css'
 
+import axios from 'axios'
+
+Vue.prototype.$http = axios
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
 Vue.config.productionTip = false
 
 new Vue({
